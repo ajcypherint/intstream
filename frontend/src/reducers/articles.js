6755655d3853b59ecefcd97ec59adb6dfb36f4e7
guzzi <guzzijones12@gmail.com>
2019-12-09 22:12:20 -0500

    initial children

--- frontend/src/reducers/articles.js
+++ frontend/src/reducers/articles.js
@@ -9,10 +9,13 @@ let START = new Date();
 START.setHours(0,0,0,0);
 
 let END= new Date();
-END.setHours(23,59,59,999);
+END.setHours(23, 59, 59, 999);
 
+// use articlestmp to load pages and retrieve data
 const initialState ={
   articles:[],
+  articlestmp:[],
+  children:{},
   loading:false,
   totalcount:0,
   errors: {},
