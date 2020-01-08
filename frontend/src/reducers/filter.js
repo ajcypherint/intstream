5709a31af93cc17d23501ea397c97fcadfdd7d05
guzzi <guzzijones12@gmail.com>
2020-01-07 22:31:51 -0500

    set default to zero for clustering

--- frontend/src/reducers/filter.js
+++ frontend/src/reducers/filter.js
@@ -16,7 +16,7 @@ const initialState ={
       startDate: START,
       endDate: END,
       sourceChosen:'',
-      threshold:0,
+      threshold:"0",
       loadSources:false,
       page:1,
       ordercol:'',
