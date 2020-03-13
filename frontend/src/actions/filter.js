7848d0fa2690c6c24f1b35464e4a173e6dae131f
guzzi <guzzijones12@gmail.com>
2020-03-12 22:57:16 -0400

    fix filter

--- frontend/src/actions/filter.js
+++ frontend/src/actions/filter.js
@@ -84,7 +84,7 @@ export const filterChange = (newSelections, path='filter', parent)=>{
       "&source__active=true"+
       "&prediction__mlmodel="+selections.modelChosen+
        (selections.modelChosen !== "" ?
-      "&prediction__mlmodel__active=true": "")
+      "&prediction__mlmodel__active=true&prediction__target=true": "")
     
     //fetch sources and models; * not just sources but all filters not inc dates *
     // could ignore this for child
