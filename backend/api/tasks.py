cf3de3c0cc7a44f355f520fa025a5559f15ec131
guzzi <guzzijones12@gmail.com>
2020-03-18 19:04:51 -0400

    add todo

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -96,6 +96,7 @@ async def fetch(url):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             #todo(aj) if 301 get redirect.  use gcaptain.com rss feed to test
+            # todo cdc.gov seems to be giving pdfs...?
             try:
                 return await response.text()
             except UnicodeDecodeError as e:
