2df8e2c9374e87a572803b59b49f283fa17b5cb6
guzzi <guzzijones12@gmail.com>
2020-07-27 17:08:21 +0000

    initial work register

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -53,6 +53,7 @@ class UserSerializer(serializers.ModelSerializer):
 
         model = UserIntStream
 
+
 class SuperUserSerializer(serializers.ModelSerializer):
     class Meta:
         fields=[
@@ -395,6 +396,31 @@ class UserSerializerUpdate(serializers.ModelSerializer):
         p = user.password
         user.set_password(p)
         user.save()
+        return user
+
+class UserSerializerRegister(serializers.ModelSerializer):
+    class Meta:
+        model = UserIntStream
+        fields = ('id',
+                  'username',
+                  'password',
+                  'email',
+                  'first_name',
+                  'last_name')
+        write_only_fields = ('password',)
+        read_only_fields = ('id',)
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
         return user
 
 class WordDocxSerializer(serializers.ModelSerializer):
