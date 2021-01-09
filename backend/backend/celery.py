2cf40a218073f35f9f23750817c70076bdb1602b
guzzi <guzzijones12@gmail.com>
2021-01-09 18:09:13 +0000

    setup periodic task schedule for each org

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -11,6 +11,8 @@ celery_app = Celery('backend',
                     backend="api.backend.OrgDatabaseBackend",
                     include = ['api.tasks'])
 
+from api import models
+from api import tasks
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
@@ -25,27 +27,51 @@ celery_app.autodiscover_tasks()
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
-#todo(aj) job to remove indicatoripv4 and indicatoripv6 after ttl
-# calculation = upload_date + ttl < today
 
-celery_app.conf.beat_schedule = {
-    # assign to single worker with concurrency = 1
-    "rss_sources":{
-        "task": "api.tasks.process_rss_sources",
-        "kwargs":{"organization_id":1},
-     #   "schedule": crontab(hour="*/1", minute="2") # every hour at 2 mins
-       "schedule": crontab(hour="*", minute="*/1") # testing
-    },
-    'clean_history_freemium': {
-        "task": "api.tasks.remove_old_articles",
-        "kwargs":{"organization_id":1},
-        "schedule": crontab(hour="1", minute="30") # every day at hour 1
-    },
-    'update_tld': {
-        "task": "api.tasks.update_suffixes",
-        "kwargs":{"organization_id":1},
-        "schedule": crontab(day_of_week="0", hour="1", minute="15")
-    }
-}
+@celery_app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    orgs = models.Organization.objects.all()
+    ids = [org.id for org in orgs]
+
+    # Calls test('hello') every 10 seconds.
+    for i in ids:
+        # test
+        sender.add_periodic_task(crontab(hour="*", minute="*/1"),
+                                 tasks.process_rss_sources.s(organization_id=i),
+                                 name='add every 1 process rss')
+        # prod
+        #sender.add_periodic_task(crontab(hour="*/1", minute="2"),
+        #                         tasks.process_rss_sources.s(organization_id=i),
+        #                         name='every hour process rss')
+
+    for i in ids:
+        sender.add_periodic_task(crontab(hour="1", minute="30"),
+                                 tasks.remove_old_articles.s(organization_id=i),
+                                 name='clean history freemium')
+
+    for i in ids:
+        sender.add_periodic_task(crontab(day_of_week="0", hour="1", minute="30"),
+                                 tasks.update_suffixes.s(organization_id=i),
+                                 name='update tld')
+
+#celery_app.conf.beat_schedule = {
+#    # assign to single worker with concurrency = 1
+#    "rss_sources":{
+#        "task": "api.tasks.process_rss_sources",
+#        "kwargs":{"organization_id":1},
+#     #   "schedule": crontab(hour="*/1", minute="2") # every hour at 2 mins
+#       "schedule": crontab(hour="*", minute="*/1") # testing
+#    },
+#    'clean_history_freemium': {
+#        "task": "api.tasks.remove_old_articles",
+#        "kwargs":{"organization_id":1},
+#        "schedule": crontab(hour="1", minute="30") # every day at hour 1
+#    },
+#    'update_tld': {
+#        "task": "api.tasks.update_suffixes",
+#        "kwargs":{"organization_id":1},
+#        "schedule": crontab(day_of_week="0", hour="1", minute="15")
+#    }
+#}
 
 
